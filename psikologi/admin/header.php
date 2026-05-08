8effa00062ff93cdccb49397845e461aa5551822
Fahmi Febriansyah <fahmijha12@gmail.com>
2026-05-08 15:45:16 +0700

    edit fitur agoysss

--- psikologi/admin/header.php
+++ psikologi/admin/header.php
@@ -17,7 +17,7 @@ if(!isset($_SESSION['id_admin'])) {
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo filemtime(__DIR__.'/assets/css/style.css'); ?>">
 </head>
 <body>
 
--- psikologi/user/header.php
+++ psikologi/user/header.php
@@ -22,7 +22,7 @@ $user_initial = strtoupper(substr($user_nama, 0, 1));
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;500;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo filemtime(__DIR__.'/assets/css/style.css'); ?>">
     <?php if(isset($extra_css)) echo $extra_css; ?>
 </head>
 <body>
