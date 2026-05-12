2dfa4fbc63700772f05c5f336728368263537e05
Fahmi Febriansyah <fahmijha12@gmail.com>
2026-05-12 13:35:49 +0700

    add noindex for other domains

--- sistempakar/header.php
+++ sistempakar/header.php
@@ -9,6 +9,7 @@ if (session_status() == PHP_SESSION_NONE) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>DPM Expert System - Diagnosa Kendaraan</title>
+    <meta name="robots" content="noindex, nofollow">
     <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <style>
